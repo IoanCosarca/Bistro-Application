a197e4b3bcd18495e322824a06c0b40a54f1f23f
ioan.cosarca <ionutcosarca1@gmail.com>
2023-08-03 11:25:32 +0300

    Initial Commit

---
+++ src/test/java/com/ntt/bistroapplication/BistroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntt.bistroapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BistroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
