4a462ebb4561496246b15179cf2fd1c54b7fbbda
JohnC <ionutcosarca1@gmail.com>
2023-09-08 13:16:47 +0300

    Implemented the test class for Orders

--- src/test/java/com/ntt/bistroapplication/controller/v1/OrderControllerTest.java
+++ src/test/java/com/ntt/bistroapplication/controller/v1/OrderControllerTest.java
@@ -1,26 +1,59 @@
 package com.ntt.bistroapplication.controller.v1;
 
-import org.junit.jupiter.api.AfterEach;
+import com.ntt.bistroapplication.exception.RestResponseEntityExceptionHandler;
+import com.ntt.bistroapplication.model.CustomerDTO;
+import com.ntt.bistroapplication.model.PlacedOrderDTO;
+import com.ntt.bistroapplication.service.OrderService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class OrderControllerTest {
+    @Mock
+    OrderService orderService;
+    @InjectMocks
+    OrderController orderController;
+    MockMvc mockMvc;
+    static final String CUSTOMER_NAME = "Mircea";
+    static final Long ID = 1L;
 
     @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void getCustomerOrders() {
+    void setUp()
+    {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(orderController)
+                .setControllerAdvice(new RestResponseEntityExceptionHandler())
+                .build();
     }
 
     @Test
-    void getTopN() {
+    void getCustomerOrders() throws Exception
+    {
+        List<PlacedOrderDTO> placedOrderDTOS = new ArrayList<>();
+        PlacedOrderDTO order = new PlacedOrderDTO();
+        CustomerDTO customer = new CustomerDTO(CUSTOMER_NAME);
+        order.setCustomer(customer);
+        orderService.addOrder(order);
+        placedOrderDTOS.add(order);
+
+        when(orderService.getCustomerOrders(ID)).thenReturn(placedOrderDTOS);
+        mockMvc.perform(get(OrderController.BASE_URL + "/customerOrders/" + ID)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isFound())
+                .andExpect(jsonPath("$", hasSize(1)));
     }
 }
